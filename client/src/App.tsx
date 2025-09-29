fa6e7cec243ef50066f0774aaa60e4089d5fc1be
SamiBenAmara <samibenamara23@gmail.com>
2025-09-29 15:06:22 -0700

    Commit base project

---
+++ client/src/App.tsx
@@ -0,0 +1,16 @@
+// import { useState } from 'react'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
+import './App.css'
+// import LandingPage from './components/LandingPage/LandingPage'
+
+function App() {
+  // const [count, setCount] = useState(0)
+
+  return (
+    <h1>Hello</h1>
+    // <LandingPage />      
+  )
+}
+
+export default App
---
+++ server/src/main/java/com/coding_site/Test.java
@@ -0,0 +1,18 @@
+package com.coding_site;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/test")
+public class Test {
+
+    @GetMapping
+    public ResponseEntity<String> testRequest() {
+        return new ResponseEntity<String>("THIS WORKS", HttpStatus.ACCEPTED);
+    }
+
+}
